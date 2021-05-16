7a712dbcae0c96bf4ecc6874bae55889e761e7aa
Mátyás Antal <latna.matyas@gmail.com>
2021-05-16 11:00:25 +0200

    jokes CRUD

---
+++ HajosTeszt/HajosTeszt/Controllers/JokeControllerNEW.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace HajosTeszt.Controllers
+{
+    [Route("api/jokesNew")]
+    [ApiController]
+    public class JokeControllerNEW : ControllerBase
+    {
+        // GET: api/jokess
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            FunnyDatabaseContext context = new FunnyDatabaseContext();
+            return context.Jokes.ToList();
+        }
+
+        // GET api/jokess/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            FunnyDatabaseContext context = new FunnyDatabaseContext();
+            var keresettVicc = (from x in context.Jokes
+                               where x.JokeSk == id
+                               select x).FirstOrDefault();
+            return keresettVicc;
+        }
+
+        // POST api/jokess
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+            FunnyDatabaseContext context = new FunnyDatabaseContext();
+            context.Jokes.Add(újVicc);
+            context.SaveChanges();
+        }
+
+        // PUT api/<JokeControllerNEW>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/jokess/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            FunnyDatabaseContext context = new FunnyDatabaseContext();
+            var törlendőVicc = (from x in context.Jokes
+                                where x.JokeSk == id
+                                select x).FirstOrDefault();
+            context.Remove(törlendőVicc);
+            context.SaveChanges();
+        }
+    }
+}
